fbfe685fccb5ee3e679bd40a6c732dd488db5ba3
Abhishek <abhitiwari3010@gmail.com>
2023-12-27 21:50:40 +0530

    made index page null and connected the dynamic route

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,17 +6,12 @@ import AddTodo from '../components/AddTodo';
 import { GetServerSideProps } from 'next/types';
 import { connectToDatabase } from '@/db/connectDb';
 import { parse } from 'cookie';
+import middleware from '@/utils/middleware';
 
 
-const Home: React.FC = () => {
+const Index: React.FC = () => {
 
-  return (
-    <div>
-      <Header />
-      <TodoList />
-      <AddTodo />
-    </div>
-  );
+  return null;
 };
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
@@ -37,9 +32,42 @@ if(!jwtToken)
 }
 
 
+  // Decode the JWT token to get the username
+  const decodedToken = await decodeJwtToken(jwtToken);
+  const username = decodedToken || '';
+  console.log("userside"+username);
+  // Redirect to the home page with the username in the URL
   return {
-    props: {},
+    redirect: {
+      destination: `/${username}`,
+      permanent: false,
+    },
   };
 };
 
-export default Home;
+const decodeJwtToken = async (jwtToken: string) => {
+  let username = '';
+  
+  console.log("this is from jwtToken");
+  console.log(jwtToken);
+  if (jwtToken) {
+    const decodedUser = await middleware("Bearer "+jwtToken);
+    console.log("this is decoded");
+  
+    if (typeof decodedUser === 'string') {
+      console.error('Unexpected string format for decoded user');
+    } else if (decodedUser && decodedUser.username) {
+      username = decodedUser.username;
+      console.log(`Username: ${username}`);
+    } else {
+      console.error('Username not found in decoded user object');
+    }
+  } else {
+    console.error('JWT token not found');
+  }
+  console.log("serverside"+username)
+  return username; // Replace with actual decoding logic
+};
+
+
+export default Index;
